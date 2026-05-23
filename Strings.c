8e9f874496e149ce4b9a3c349dcd0dc94a5f48a8
fsorzzoni <ferminsorzzoni@gmail.com>
2026-05-23 17:10:27 -0300

    cocnatenate, power y reverse enteras

--- Strings.c
+++ Strings.c
@@ -24,12 +24,66 @@ int Contains(const char *str, const char ch) {
 
 // OPERACION A DEFINIR
 
-char* Concatenate(const char *str1, const char *str2) {
+char* Concatenate(const char *stringArray[], const int arrayLength) {
+    // empieza en 1 ya que al final esta el '\0'
+    int totalLength = 1;
 
+    for(int i = 0; i < arrayLength; i++) {
+        totalLength += GetLength(stringArray[i]);
+    }
+
+    char *concatenatedString = malloc(totalLength * sizeof(char));
+
+    int currentLength = 0;
+
+    for(int i = 0; i < arrayLength; i++) {
+        const char *string = stringArray[i];
+        
+        for(int j = 0; string[j] != '\0'; j++) {
+            concatenatedString[currentLength] = string[j];
+            currentLength++;
+        }
+    }
+
+    concatenatedString[currentLength] = '\0';
+
+    return concatenatedString;
 }
 
-char* Power(const char *str, const int times) {
+char* Power(const char *string, const int power) {
+    const int stringLength = GetLength(string);
 
+    char *poweredString = malloc(((stringLength * power) + 1) * sizeof(char));
+
+    int currentLength = 0;
+
+    for(int i = 0; i < power; i++) {
+        for(int j = 0; j < stringLength; j++) {
+            poweredString[currentLength] = string[j];
+            currentLength++;
+        }
+    }
+
+    poweredString[currentLength] = '\0';
+
+    return poweredString;
 }
 
-// OPERACION A DEFINIR
+// OPERACION EXTRA
+
+char* Reverse(const char *string) {
+    const int stringLength = GetLength(string);
+
+    char *reversedString = malloc((stringLength + 1) * sizeof(char));
+
+    int currentLength = 0;
+
+    while(currentLength < stringLength) {
+        reversedString[currentLength] = string[stringLength - 1 - currentLength];
+        currentLength++;
+    }
+
+    reversedString[currentLength] = '\0';
+    
+    return reversedString;
+}
