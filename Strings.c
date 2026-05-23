b1b61caa694ee6a0b33dd9dedcefc51e423ed307
fsorzzoni <ferminsorzzoni@gmail.com>
2026-05-23 16:51:57 -0300

    cambios de nombres

---
+++ Strings.c
@@ -0,0 +1,35 @@
+#include "Strings.h"
+#define TRUE 1
+#define FALSE 0
+
+int IsEmpty(const char *str) {
+
+}
+
+int GetLength(const char *str) {
+
+}
+
+int AreEqual(const char *str1, const char *str2) {
+
+}
+
+int AreDecimalDigits(const char *str) {
+
+}
+
+int Contains(const char *str, const char ch) {
+
+}
+
+// OPERACION A DEFINIR
+
+char* Concatenate(const char *str1, const char *str2) {
+
+}
+
+char* Power(const char *str, const int times) {
+
+}
+
+// OPERACION A DEFINIR
