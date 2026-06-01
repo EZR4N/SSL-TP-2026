064f1df77f24e8b1f49916cc6ba9828da95e47c8
TomasLucasUTN <tlucas@frba.utn.edu.ar>
2026-06-01 10:46:57 -0300

    Implementacion de todosiguales.c y suma.c(falta toInteger)

--- suma.c
+++ suma.c
@@ -1,3 +1,26 @@
-int main(int argc, char *argv[]) {
+#include <stdio.h>
+#include "Strings.h"
+#include "Conversion.h" 
+
+int main(int argc, char **argv) {
+    int suma_total = 0;
+
+    for (char **arg = argv + 1; *arg != NULL; arg++) {
+        char *cadena = *arg;
+        int signo = 1;
+
+        if (*cadena == '-') {
+            signo = -1;
+            cadena++; 
+        } else if (*cadena == '+') {
+            cadena++; 
+        }
+
+        if (AreDecimalDigits(cadena)) {
+            // suma_total += (ToInteger(cadena) * signo);
+        }
+    }
+
+    printf("%d\n", suma_total);
     return 0;
 }
