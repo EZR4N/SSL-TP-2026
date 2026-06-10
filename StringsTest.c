fcbebdf6bfcbde58b0764e763a64298aa68d6e6a
Sebastian-Faingerch <101648802+sebapincha11@users.noreply.github.com>
2026-06-10 20:28:53 -0300

    FEAT: Test y especificación de las funciones de Strings.c menos Concatenate, Power y Reverse

--- StringsTest.c
+++ StringsTest.c
@@ -2,6 +2,58 @@
 #include "Strings.h"
 #include <assert.h>
 
+void TestIsEmpty() {
+    // string vacio devuelve verdadero
+    assert(IsEmpty(""));
+
+    // string con caracteres devuelve falso
+    assert(!IsEmpty("Hola"));
+}
+
+void TestGetLength() {
+    // string vacio tiene longitud 0
+    assert(GetLength("") == 0);
+
+    // string con caracteres
+    assert(GetLength("Hola") == 4);
+}
+
+void TestAreEqual() {
+    // dos strings vacios son iguales
+    assert(AreEqual("", ""));
+
+    // strings iguales
+    assert(AreEqual("Hola", "Hola"));
+
+    // strings distintos con igual longitud
+    assert(!AreEqual("Hola", "Chau"));
+
+    // strings con distinta longitud
+    assert(!AreEqual("Hola", "Hol"));
+}
+
+void TestAreDecimalDigits() {
+    // string vacio devuelve verdadero
+    assert(AreDecimalDigits(""));
+
+    // string solo con digitos
+    assert(AreDecimalDigits("12345"));
+
+    // string con letras o caracteres invalidos
+    assert(!AreDecimalDigits("123a5"));
+}
+
+void TestContains() {
+    // string vacio no contiene nada
+    assert(!Contains("", 'a'));
+
+    // string contiene el caracter
+    assert(Contains("Hola", 'a'));
+
+    // string no contiene el caracter
+    assert(!Contains("Hola", 'z'));
+}
+
 void TestConcatenate() {
     // si todos los strings son vacios, se devuelve string vacio
     assert(AreEqual(Concatenate((const char*[]){"", ""}, 2), ""));
@@ -39,6 +91,11 @@ void TestReverse() {
 }
 
 void TestStrings() {
+    TestIsEmpty();
+    TestGetLength();
+    TestAreEqual();
+    TestAreDecimalDigits();
+    TestContains();
     TestConcatenate();
     TestPower();
     TestReverse();
