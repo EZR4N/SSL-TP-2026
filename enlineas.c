0582f33ef938b834f57a25bc075905ad2f87d3c1
fsorzzoni <ferminsorzzoni@gmail.com>
2026-06-10 19:56:29 -0300

    enlineas.c

--- enlineas.c
+++ enlineas.c
@@ -1,3 +1,9 @@
+// interpreto que lo que se debe hacer es imprimir cada uno en una linea
+
 int main(int argc, char *argv[]) {
+    for(int i = 1; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
     return 0;
 }
