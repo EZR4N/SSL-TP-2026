bb6d1cd152380965bc2ef7d8411f8c8f974cb039
MartinnRomero <romero.martin.nahuel8@gmail.com>
2026-06-10 17:26:59 -0300

    feat funcion ToInteger y asserts

--- Conversion.c
+++ Conversion.c
@@ -1,5 +1,17 @@
 #include "Conversion.h"
 
 int ToInteger(char *str) {
+    int resultado = 0;
+    int signo = 1;
 
+    for (int i = 0; str[i] != '\0'; i++) {
+      if (i == 0 && str[0] == '-') {
+            signo = -1;
+            i = 1;
+        }
+    
+        resultado = resultado * 10 + (str[i] - '0');
+    }
+
+    return signo * resultado;
 }
--- ConversionTest.c
+++ ConversionTest.c
@@ -1,3 +1,12 @@
+#include "ConversionTest.h"
+#include "Conversion.h"
+#include <assert.h>
+
 void TestConversion() {
-    
+    assert(ToInteger("-1") == -1);
+    assert(ToInteger("-255") == -255);
+    assert(ToInteger("-0") == 0);
+    assert(ToInteger("0") == 0);
+    assert(ToInteger("1") == 1);
+    assert(ToInteger("255") == 255);
 }
